055f52cda48ef79e82ba4058c79b6017eed6e0c6
lipenghao <972208640@qq.com>
2022-09-24 01:39:00 +0800

    demo

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function fast() {
       'Accept-Encoding': 'gzip,deflate,br',
       'Connection': 'keep-alive',
       'User-Agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1',
-      't': 'xxx'
+      't': '17361736187'
     }
   })
     .then(res => {
